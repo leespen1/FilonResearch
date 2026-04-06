dd4570e3ad5a5d43c9fed9a5866a3740843c7aa0
Spencer Lee <leespen1@msu.edu>
2026-04-06 17:55:18 -0400

    Updated FilonResearch.jl to include new hard-coded methods.

--- src/FilonResearch.jl
+++ src/FilonResearch.jl
@@ -2,7 +2,7 @@ module FilonResearch
 
 import Polynomials
 import Polynomials: Polynomial, derivative
-import LinearAlgebra: dot, mul!
+import LinearAlgebra: dot, mul!, I, Diagonal, cond, eigvals
 import LinearMaps: LinearMap
 import IterativeSolvers: gmres
 
@@ -24,5 +24,13 @@ include("filon_timestep.jl")
 export filon_timestep, Algorithm1, filon_solve, FilonProblem, get_LHS_mat, filon_timestep_integral_error
 include("controlled_operators.jl")
 export ControlledFunctionOp, ControlledOp
+include("hardcoded_lhs_rhs.jl")
+export filon_timestep_s0_backslash, filon_timestep_s0_gmres
+export filon_timestep_s1_backslash, filon_timestep_s1_gmres
+export filon_timestep_s2_backslash, filon_timestep_s2_gmres
+export filon_solve_hardcoded, filon_S_plus_S_minus
+
+include("spencer_hardcoded_lhs_rhs.jl")
+export Ws_explicit_implicit, S_explicit_implicit_filon, S_explicit_implicit_filon_s0, S_analysis_filon_s0, S_explicit_implicit_filon_s1, S_analysis_filon_s1
 
 end # module FilonResearch
